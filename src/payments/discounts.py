a294903c21f71f3ece359334f81c67ee6d271743
nidhihk003 <shpnidhi03@gmail.com>
2026-02-10 15:04:19 +0530

    Update discounts.py

--- src/payments/discounts.py
+++ src/payments/discounts.py
@@ -1,1 +1,4 @@
-
+def apply_discount(amount: float, percentage: float) -> float:
+    if percentage < 0 or percentage > 100:
+        raise ValueError("Invalid discount")
+    return amount * (1 - percentage / 100)
